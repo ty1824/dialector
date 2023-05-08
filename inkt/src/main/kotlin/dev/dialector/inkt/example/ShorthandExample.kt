bb1e553041973a33589ea61245b0c50de919827b
tyler.hodgkins <tyler.hodgkins@workday.com>
2023-05-07 23:55:30 -0700

    Rework new query DSL and implement more robust incrementalization

--- inkt/src/main/kotlin/dev/dialector/inkt/example/ShorthandExample.kt
+++ inkt/src/main/kotlin/dev/dialector/inkt/example/ShorthandExample.kt
@@ -1,8 +1,10 @@
 package dev.dialector.inkt.example
 
-import dev.dialector.inkt.example.HelloWorldShort.inputString
+import dev.dialector.inkt.example.HelloWorldShort.inputs
 import dev.dialector.inkt.example.HelloWorldShort.length
 import dev.dialector.inkt.example.HelloWorldShort.longest
+import dev.dialector.inkt.example.HelloWorldShort.totalLength
+import dev.dialector.inkt.next.DatabaseContext
 import dev.dialector.inkt.next.QueryDatabaseImpl
 import dev.dialector.inkt.next.defineQuery
 
@@ -13,7 +15,12 @@ internal object HelloWorldShort {
      *
      * A query database will need to provide means to set these input values.
      */
-    val inputString by defineQuery<String, String>()
+    val inputs by defineQuery<List<Pair<String, String>>>()
+
+
+    val inputString by defineQuery<String, String?> { key: String ->
+        query(inputs).find { it.first == key }?.second
+    }
 
     /**
      * Derived queries are composed of other queries - in this case, [length] returns the length
@@ -21,7 +28,7 @@ internal object HelloWorldShort {
      */
     val length by defineQuery { key: String ->
         println("Recomputing length for $key")
-        inputString(key).length
+        query(inputString, key)?.length
     }
 
     /**
@@ -29,45 +36,62 @@ internal object HelloWorldShort {
      */
     val longest by defineQuery { keys: Set<String> ->
         println("Recomputing longest")
-        keys.maxByOrNull { length(it) }?.let { inputString(it) }
+        keys.maxByOrNull { query(length, it) ?: 0 }?.let { query(inputString, it) }
+    }
+
+    val totalLength by defineQuery<Int> {
+        query(inputs).sumOf { query(length, it.first) ?: 0 }
     }
 }
 
 internal fun main() {
     val db = QueryDatabaseImpl()
-    db.run {
-        inputString["foo"] = "hello"
-        inputString.remove("foo")
-
-        println("foo: Length is ${length("foo")}")
-        println("foo: Length is ${length("foo")} shouldn't recompute!")
-
-        inputString["bar"] = "bye"
-
-        println("foo: Length is ${length("foo")} shouldn't recompute!")
-        println("bar: Length is ${length("bar")}")
-        println("bar: Length is ${length("bar")} shouldn't recompute!")
-
-        inputString["foo"] = "longer"
-
-        println("foo: Length is ${length("foo")}")
-        println("bar: Length is ${length("bar")} shouldn't recompute!")
-
-        println("longest {foo, bar} is: ${longest(setOf("foo", "bar"))}")
-        println("longest {foo, bar} is: ${longest(setOf("foo", "bar"))}")
-
-        inputString["bar"] = "even longer"
-        println("longest {foo, bar} is: ${longest(setOf("foo", "bar"))}")
-        println("longest {foo, bar} is: ${longest(setOf("foo", "bar"))}")
-
-        inputString["baz"] = "definitely the longest"
-        println("longest {foo, bar, baz} is ${longest(setOf("foo", "bar", "baz"))}")
-        println("longest {foo, bar, baz} is ${longest(setOf("foo", "bar", "baz"))}")
-
-        inputString["foo"] = "long"
-        inputString["bar"] = "med"
-        inputString["baz"] = "s"
-        println("longest {foo, bar, baz} is ${longest(setOf("foo", "bar", "baz"))}")
-        println("longest {foo, bar, baz} is ${longest(setOf("foo", "bar", "baz"))}")
+    db.writeTransaction {
+        logic()
+        logic()
+        logic()
     }
+}
+
+private fun DatabaseContext.logic() {
+    set(inputs, listOf("foo" to "hello"))
+//        set(inputString, "foo", "hello")
+    println("foo: Length is ${query(length, "foo")}")
+    println("foo: Length is ${query(length, "foo")} shouldn't recompute!")
+
+    set(inputs, listOf("foo" to "hello", "bar" to "bye"))
+//        set(inputString, "bar", "bye")
+
+    println("foo: Length is ${query(length, "foo")} shouldn't recompute!")
+    println("bar: Length is ${query(length, "bar")}")
+    println("bar: Length is ${query(length, "bar")} shouldn't recompute!")
+
+    set(inputs, listOf("foo" to "longer", "bar" to "bye"))
+//        set(inputString, "foo", "longer")
+
+    println("foo: Length is ${query(length, "foo")}")
+    println("bar: Length is ${query(length, "bar")} shouldn't recompute!")
+
+    println("longest {longer, bye} is: ${query(longest, setOf("foo", "bar"))}")
+    println("longest {longer, bye} is: ${query(longest, setOf("foo", "bar"))} shouldn't recompute!")
+
+    set(inputs, listOf("foo" to "longer", "bar" to "even longer"))
+//        inputString["bar"] = "even longer"
+    println("longest {foo, bar} is: ${query(longest, setOf("foo", "bar"))}")
+    println("longest {foo, bar} is: ${query(longest, setOf("foo", "bar"))}")
+    println("total length: ${query(totalLength)}")
+
+    set(inputs, listOf("foo" to "longer", "bar" to "even longer", "baz" to "definitely the longest"))
+//        inputString["baz"] = "definitely the longest"
+    println("longest {foo, bar, baz} is ${query(longest, setOf("foo", "bar", "baz"))}")
+    println("longest {foo, bar, baz} is ${query(longest, setOf("foo", "bar", "baz"))}")
+    println("total length: ${query(totalLength)}")
+
+    set(inputs, listOf("foo" to "long", "bar" to "med", "baz" to "s"))
+//        inputString["foo"] = "long"
+//        inputString["bar"] = "med"
+//        inputString["baz"] = "s"
+    println("longest {foo, bar, baz} is ${query(longest, setOf("foo", "bar", "baz"))}")
+    println("longest {foo, bar, baz} is ${query(longest, setOf("foo", "bar", "baz"))}")
+    println("total length: ${query(totalLength)}")
 }
--- inkt/src/main/kotlin/dev/dialector/inkt/next/QueryDsl.kt
+++ inkt/src/main/kotlin/dev/dialector/inkt/next/QueryDsl.kt
@@ -3,26 +3,48 @@ package dev.dialector.inkt.next
 import kotlin.properties.PropertyDelegateProvider
 import kotlin.properties.ReadOnlyProperty
 
-public interface QueryContext {
-    /**
-     * Runs this query for the given input.
-     */
-    public operator fun <K, V> QueryDefinition<K, V>.invoke(key: K): V
-}
-
-/**
- * Defines a query with the given implementation.
- */
-public fun <K, V> defineQuery(
-    name: String? = null,
-    implementation: QueryContext.(K) -> V = { throw NotImplementedError("Query not implemented") }
-): PropertyDelegateProvider<Any?, ReadOnlyProperty<Any?, QueryDefinition<K, V>>> =
-    QueryDefinitionInitializer(name, implementation)
 
 /**
  * A query that Inkt can track and incrementalize.
  */
-public interface QueryDefinition<K, V> {
+public interface QueryDefinition<K : Any, V> {
     public val name: String
+
+    /**
+     * Runs the logic for this query directly. Will not perform any caching.
+     */
     public fun execute(context: QueryContext, key: K): V
 }
+
+/**
+ * A context that provides the ability to run queries.
+ */
+public interface QueryContext {
+    /**
+     * Runs a query for the given input.
+     */
+    public fun <K : Any, V> query(definition: QueryDefinition<K, V>, key: K): V
+
+    /**
+     * Runs a no-argument query for the given input.
+     */
+    public fun <V> query(definition: QueryDefinition<Unit, V>): V = query(definition, Unit)
+}
+
+/**
+ * Defines a no-argument query with the given implementation.
+ */
+public fun <V : Any> defineQuery(
+    name: String? = null,
+    implementation: QueryContext.() -> V = { throw NotImplementedError("Query not implemented") }
+): PropertyDelegateProvider<Any?, ReadOnlyProperty<Any?, QueryDefinition<Unit, V>>> =
+    QueryDefinitionInitializer(name) { implementation() }
+
+/**
+ * Defines a query with the given implementation.
+ */
+public fun <K : Any, V> defineQuery(
+    name: String? = null,
+    implementation: QueryContext.(K) -> V = { throw NotImplementedError("Query not implemented") }
+): PropertyDelegateProvider<Any?, ReadOnlyProperty<Any?, QueryDefinition<K, V>>> =
+    QueryDefinitionInitializer(name, implementation)
